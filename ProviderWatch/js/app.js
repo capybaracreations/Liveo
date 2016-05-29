143819cc04ef80c23f81cfc41ef9ce3aab704adb
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2016-05-29 03:28:17 +0200

    first version

--- ProviderWatch/js/app.js
+++ ProviderWatch/js/app.js
@@ -1,34 +1,6 @@
-/*
-* Copyright (c) 2015 Samsung Electronics Co., Ltd.
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are
-* met:
-*
-* * Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* * Redistributions in binary form must reproduce the above
-* copyright notice, this list of conditions and the following disclaimer
-* in the documentation and/or other materials provided with the
-* distribution.
-* * Neither the name of Samsung Electronics Co., Ltd. nor the names of its
-* contributors may be used to endorse or promote products derived from
-* this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 
+var lasthr = -5;
+var isVibrateEnabled = false;
 var SAAgent,
     SASocket,
     connectionListener,
@@ -37,29 +9,63 @@ var SAAgent,
 /* Make Provider application running in background */
 tizen.application.getCurrentApplication().hide();
 
-function createHTML(log_string)
+window.onload = function () {
+	$("#bodyDiv").load("home.html");
+	window.webapis.motion.start("HRM", onchangedCB);
+};
+
+function receiveData(channelId, data) {
+	//data
+    if (!SAAgent.channelIds[0]) {
+        return;
+    }
+
+    if (data == "EXIT") exit();
+    else if (data == "ALERT") {
+    	$("#bodyDiv").load("alert.html");
+    	isVibrateEnabled = true;
+    } else if (data == "ALERT_CANCEL") {
+    	$("#bodyDiv").load("home.html");
+    	isVibrateEnabled = false;
+    } else SASocket.sendData(SAAgent.channelIds[0], lasthr);
+}
+
+
+function onchangedCB(hrmInfo) 
 {
-    var content = document.getElementById("toast-content");
-    content.innerHTML = log_string;
-    tau.openPopup("#toast");
+	if (isVibrateEnabled == true) navigator.vibrate(50);
+	lasthr = hrmInfo.heartRate;
+}
+
+function exit() {
+	webapis.motion.stop("HRM");
+	try {
+		if (SASocket != null) {
+			SASocket.close();
+			SASocket = null;
+			log("closeConnection");
+		}
+	} catch(err) {
+		log("exception [" + err.name + "] msg[" + err.message + "]");
+	}
+
+	tizen.application.getCurrentApplication().exit();
+}
+
+function onAlertButtonClick() {
+	SASocket.sendData(SAAgent.channelIds[0], "ALERT_CANCEL");
 }
 
 connectionListener = {
     /* Remote peer agent (Consumer) requests a service (Provider) connection */
     onrequest: function (peerAgent) {
 
-        createHTML("peerAgent: peerAgent.appName<br />" +
-                    "is requsting Service conncetion...");
-
         /* Check connecting peer by appName*/
         if (peerAgent.appName === "HelloAccessoryConsumer") {
             SAAgent.acceptServiceConnectionRequest(peerAgent);
-            createHTML("Service connection request accepted.");
 
         } else {
             SAAgent.rejectServiceConnectionRequest(peerAgent);
-            createHTML("Service connection request rejected.");
-
         }
     },
 
@@ -67,8 +73,6 @@ connectionListener = {
     onconnect: function (socket) {
         var onConnectionLost,
             dataOnReceive;
-
-        createHTML("Service connection established");
 
         /* Obtaining socket */
         SASocket = socket;
@@ -81,22 +85,12 @@ connectionListener = {
         SASocket.setSocketStatusListener(onConnectionLost);
 
         dataOnReceive =  function dataOnReceive (channelId, data) {
-            var newData;
-
-            if (!SAAgent.channelIds[0]) {
-                createHTML("Something goes wrong...NO CHANNEL ID!");
-                return;
-            }
-            newData = data + " :: " + new Date();
-
-            /* Send new data to Consumer */
-            SASocket.sendData(SAAgent.channelIds[0], newData);
-            createHTML("Send massage:<br />" +
-                        newData);
+            receiveData(channelId, data);
         };
 
         /* Set listener for incoming data from Consumer */
         SASocket.setDataReceiveListener(dataOnReceive);
+    	//window.webapis.motion.start("HRM", onchangedCB);
     },
     onerror: function (errorCode) {
         createHTML("Service connection error<br />errorCode: " + errorCode);
@@ -108,8 +102,6 @@ function requestOnSuccess (agents) {
 
     for (i; i < agents.length; i += 1) {
         if (agents[i].role === "PROVIDER") {
-            createHTML("Service Provider found!<br />" +
-                        "Name: " +  agents[i].name);
             SAAgent = agents[i];
             break;
         }
@@ -128,6 +120,12 @@ function requestOnError (e) {
 /* Requests the SAAgent specified in the Accessory Service Profile */
 webapis.sa.requestSAAgent(requestOnSuccess, requestOnError);
 
+function createHTML(log_string)
+{
+    var content = document.getElementById("toast-content");
+    content.innerHTML = log_string;
+    tau.openPopup("#toast");
+}
 
 (function () {
     /* Basic Gear gesture & buttons handler */
@@ -140,7 +138,7 @@ webapis.sa.requestSAAgent(requestOnSuccess, requestOnError);
             pageid = page ? page.id : "";
             if (pageid === "main") {
                 try {
-                    tizen.application.getCurrentApplication().exit();
+                	tizen.application.getCurrentApplication().exit();
                 } catch (ignore) {
                 }
             } else {
